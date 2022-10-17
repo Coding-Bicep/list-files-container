aa5ba685f3308930c3199661d7270085cbcc9b74
qxv3587 <Mohamed.Ltaief@partner.bmw.de>
2022-10-17 15:09:57 +0200

    My first action is ready

---
+++ listfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+cd $1
+declare -a files_array
+for file in $2
+do
+    files_array+=($file)
+done
+
+# for value in "${files_array[@]}"
+# do
+#      echo $value
+# done
+
+echo "${files_array[@]}" >> $GITHUB_OUTPUT
