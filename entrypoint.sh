175d814695fb7f53dd41b99e0bf0837917533f25
qxv3587 <Mohamed.Ltaief@partner.bmw.de>
2022-12-02 20:18:34 +0100

    Add filepath to the outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ cd ${GITHUB_WORKSPACE}/modules
 declare -a files_array
 for file in $2
 do
-    filePath="$(cd "$(dirname "$file")"; pwd -P)/$(basename "$file")"
-    files_array+=($filePath)
+    #filePath="$(cd "$(dirname "$file")"; pwd -P)/$(basename "$file")"
+    files_array+=(${GITHUB_WORKSPACE}/modules/$file)
 done
 
 echo "files=${files_array[@]}" >> $GITHUB_OUTPUT
