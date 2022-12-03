15f4d596d140312087469632e51d7a783cb0d553
qxv3587 <Mohamed.Ltaief@partner.bmw.de>
2022-12-03 17:49:49 +0100

    Test github action locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+echo "Current Directory: $1"
+echo "Extension: $2"
+echo "Container Registry: $3"
+echo "Version: $4"
+
 cd ${GITHUB_WORKSPACE}/$1
 
 declare -a files_array
 for file in $2
 do
-    #filePath="$(cd "$(dirname "$file")"; pwd -P)/$(basename "$file")"
+    echo $file
     files_array+=($file)
 done
 
-echo "files=${files_array[@]}" >> $GITHUB_OUTPUT
+# echo "files=${files_array[@]}" >> $GITHUB_OUTPUT
