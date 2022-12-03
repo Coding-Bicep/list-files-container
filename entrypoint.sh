f4e0e0e3d62ff4f7804849c905e161d422d8f257
qxv3587 <Mohamed.Ltaief@partner.bmw.de>
2022-12-03 21:19:51 +0100

    Test github action locally

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Current Directory: $1"
-echo "Extension: $2"
-echo "Container Registry: $3"
-echo "Version: $4"
-echo "ClientId: $5"
-echo "Password: $6"
-echo "TennantId: : $7"
+####    Inputs for this bash script ###############
+# Current Directory: $1
+# Extension: $2
+# Container Registry: $3
+# Version: $4
+# ClientId: $5
+# Password: $6
+# TennantId: : $7
 
-cd ${GITHUB_WORKSPACE}
+# cd ${GITHUB_WORKSPACE}
 
-declare -a files_array
 for file in $2
 do
     echo $file
@@ -22,4 +22,4 @@ do
     az bicep publish -f $file --target "br:$3/$file_name_without_extension:$4"
 done
 
-# echo "files=${files_array[@]}" >> $GITHUB_OUTPUT
+echo "result=success" >> $GITHUB_OUTPUT
